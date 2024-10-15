df346abbab4fec693e443791585d5aee882f128c
vietcuong <vietcuong2k182@gmail.com>
2024-10-15 15:44:05 +0700

    15/10 - config-index

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '19012003Vietcuong@');
+define('DB_NAME', 'qldaotao');
+define('DB_POST', '3306');
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+$path_project = 'PRJ-IT3150';
+
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', $_SERVER['DOCUMENT_ROOT']. DS .$path_project);
+
+//echo 'Document Root: ' . $_SERVER['DOCUMENT_ROOT'];
+
